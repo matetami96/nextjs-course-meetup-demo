a1f05c7705de511e8604336b0c12283d23bcecb4
matetami <matetami96@gmail.com>
2021-10-12 14:39:38 +0300

    fixed fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -46,7 +46,7 @@ export async function getStaticPaths() {
 	}
 
 	return {
-		fallback: false,
+		fallback: "blocking",
 		paths: meetups.map((meetup) => ({
 			params: { meetupId: meetup._id.toString() },
 		})),
